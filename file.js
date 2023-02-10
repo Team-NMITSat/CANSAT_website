86ff89f4ade8cbea0fcd17f0f6c6b5e9b87d214b
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-02-10 13:58:15 +0530

    commit all

---
+++ file.js
@@ -0,0 +1,10 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "Hii!!     ",
+      "Welcome to NMITSAT Website!",
+    ],
+    loop: true,
+    typeSpeed: 70,
+    backSpeed: 60,
+    backDelay: 1500,
+  });
