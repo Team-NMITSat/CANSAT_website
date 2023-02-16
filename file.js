2067d5646b991121399f1b30d368240dab20d590
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-02-16 17:09:12 +0530

    commit all

--- file.js
+++ file.js
@@ -8,3 +8,13 @@ var typeData = new Typed(".role", {
     backSpeed: 60,
     backDelay: 1500,
   });
+
+  burger = document.querySelector('.burger')
+  navbar = document.querySelector('.navbar')
+  navList = document.querySelector('.nav-list')
+  rightnav = document.querySelector('.rightnav')
+  
+  burger.addEventListener('click', ()=>{
+      navList.classList.toggle('v-class-resp');
+      navbar.classList.toggle('height-nav-resp');
+  })
